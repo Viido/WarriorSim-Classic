5e44c7e2d3c5c57ed3fd1fc1f519d7ce50c6cc02
Viido Kaur Lutsar <37119942+Viido@users.noreply.github.com>
2020-03-27 15:02:12 +0200

    Added icons for application

--- src/main/java/sim/Main.java
+++ src/main/java/sim/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +13,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("talents/fxml/Talents.fxml"));
 
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("WarriorSim Classic");
         primaryStage.setScene(new Scene(root, 720, 420));
         primaryStage.getScene().getStylesheets().add(getClass().getResource("talents/css/Talents.css").toExternalForm());
+        primaryStage.getIcons().add(new Image(getClass().getResource("/images/app/warrior-wow-icon.png").toExternalForm()));
 
         primaryStage.show();
     }
